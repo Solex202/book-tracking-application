29645a0f4cea69fcdf59e0ae8110e6bb9af2c69f
Lotachukwu Onwuka <christiansolomon592@gmail.com>
2023-01-24 20:00:05 +0100

    implementing product service apis

---
+++ src/main/java/com/lotaproject/productMS/product/Product.java
@@ -0,0 +1,27 @@
+package com.lotaproject.productMS.product;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@ToString
+@Getter
+@Setter
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String description;
+    private Double price;
+    private String imageUrl;
+
+}
---
+++ src/main/java/com/lotaproject/productMS/product/ProductService.java
@@ -0,0 +1,13 @@
+package com.lotaproject.productMS.product;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+}
