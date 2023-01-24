43222b82edf24af4ce71de26902a4ab29532956f
Lotachukwu Onwuka <christiansolomon592@gmail.com>
2023-01-24 20:34:40 +0100

    implementing product service apis

--- src/main/java/com/lotaproject/productMS/product/ProductController.java
+++ src/main/java/com/lotaproject/productMS/product/ProductController.java
@@ -1,9 +1,28 @@
 package com.lotaproject.productMS.product;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/product")
 public class ProductController {
+
+    private final ProductService productService;
+
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Product>> getAllProducts(){
+        List<Product> products = productService.getProducts();
+        return new ResponseEntity<>(products, HttpStatus.OK);
+    }
 }
