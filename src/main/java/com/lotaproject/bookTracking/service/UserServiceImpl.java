5f533d010b17ef0e9a4a7a59cd716d4c4e82ba41
Lotachukwu Onwuka <christiansolomon592@gmail.com>
2023-02-01 18:31:15 +0100

    creating register user service

---
+++ src/main/java/com/lotaproject/bookTracking/service/UserServiceImpl.java
@@ -0,0 +1,39 @@
+package com.lotaproject.bookTracking.service;
+
+import com.lotaproject.bookTracking.dto.RegisterUserDto;
+import com.lotaproject.bookTracking.model.MyUser;
+import com.lotaproject.bookTracking.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class UserServiceImpl implements UserService{
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    public void registerUser(RegisterUserDto dto) {
+
+        MyUser myUser = new MyUser();
+        myUser.setUsername(dto.getUsername());
+
+        String passwordEncoder = passwordEncoder().encode(dto.getPassword());
+        log.info("ENCODED PASSWORD -------------------> {} ", passwordEncoder);
+        myUser.setPassword(passwordEncoder);
+
+        userRepository.save(myUser);
+
+    }
+}
