8cf1bea6e4099afcf80ac3d3c21b582001185b44
JacinthEsther <agbonirojacinta@gmail.com>
2023-04-07 17:33:59 +0100

    creating a jwt service to extract username

--- src/main/java/com/lotaproject/bookTracking/security/JwtService.java
+++ src/main/java/com/lotaproject/bookTracking/security/JwtService.java
@@ -2,12 +2,16 @@ package com.lotaproject.bookTracking.security;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.Map;
 import java.util.function.Function;
 
 @Service
@@ -17,14 +21,27 @@ public class JwtService {
     private final JwtUtil jwtUtil;
 
     public String extractUsername(String token) {
-        return extractClaims(token, Claims::getSubject);
+        return extractClaim(token, Claims::getSubject);
     }
 
-    public <T> T extractClaims(String token, Function<Claims, T> claimsResolver){
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver){
         final Claims claims = extractAllClaims(token);
         return  claimsResolver.apply(claims);
 
+    }
 
+    public String generateToken(
+            Map<String, Object> extraClaims,
+            UserDetails userDetails
+    ){
+        return Jwts.
+                builder().
+                setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
     }
     private Claims extractAllClaims(String token){
         return Jwts
